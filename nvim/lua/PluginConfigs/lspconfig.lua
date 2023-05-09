3f5e20c142ae41ab59a4f21dc6486e55302db2c7
kokossVN <manh12345678951235789@gmail.com>
2023-05-09 09:17:37 +0700

    little change

--- nvim/lua/PluginConfigs/lspconfig.lua
+++ nvim/lua/PluginConfigs/lspconfig.lua
@@ -1,10 +1,7 @@
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
   -- require('lspconfig')['clangd,lua-lsp,pyright,html,typescript-language-server'].setup { capabilities = capabilities }
   require('lspconfig')['clangd'].setup { capabilities = capabilities }
   require('lspconfig')['pyright'].setup { capabilities = capabilities }
   require('lspconfig')['html'].setup { capabilities = capabilities }
-  
-  
   local lspconfig = require('lspconfig')
-  lspconfig.tsserver.setup{}
+local lsp_defaults = lspconfig.util.default_config
