d40670b5923c10a1e0a35b6c9a514ab2608b59df
kokossVN <manh12345678951235789@gmail.com>
2023-05-09 11:56:25 +0700

    nvim add LSP

--- nvim/lua/PluginConfigs/lspconfig.lua
+++ nvim/lua/PluginConfigs/lspconfig.lua
@@ -1,7 +1,14 @@
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  -- require('lspconfig')['clangd,lua-lsp,pyright,html,typescript-language-server'].setup { capabilities = capabilities }
-  require('lspconfig')['clangd'].setup { capabilities = capabilities }
-  require('lspconfig')['pyright'].setup { capabilities = capabilities }
-  require('lspconfig')['html'].setup { capabilities = capabilities }
-  local lspconfig = require('lspconfig')
-local lsp_defaults = lspconfig.util.default_config
+-- require('lspconfig')['clangd,lua-lsp,pyright,html,typescript-language-server'].setup { capabilities = capabilities }
+
+local lspconfig = require('lspconfig')
+-- local lsp_defaults = lspconfig.util.default_config
+
+
+lspconfig.html.setup ({ capabilities = capabilities })
+lspconfig.clangd.setup ({ capabilities = capabilities })
+lspconfig.pyright.setup ({ capabilities = capabilities })
+lspconfig.vimls.setup ({ capabilities = capabilities })
+lspconfig.csharp_ls.setup ({ capabilities = capabilities })
+lspconfig.lua_ls.setup ({ capabilities = capabilities })
+lspconfig.tsserver.setup ({ capabilities = capabilities })
