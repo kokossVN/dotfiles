bfdf33fc07d42a52470e7517b917bcbbf1ad8647
kokossVN <manh12345678951235789@gmail.com>
2023-07-21 16:53:01 +0700

    change smt

--- bash_customalias.sh
+++ bash_customalias.sh
@@ -6,10 +6,8 @@ alias vim='nvim'
 alias lolcat='~/dotfiles/custom_bash/center.python3 | lolcat'
 alias .file='cd ~/dotfiles'
 alias discord='flatpak run com.discordapp.Discord'
-alias lazygit='git add .&& 
-	git commit -m "smail change" &&
-	git push
-'
+alias lazygit='~/dotfiles/custom_bash/lazygit.sh'
 alias ls='lsd'
 alias l='lsd -Fl'
 alias ide='~/dotfiles/custom_bash/tmux-ide.sh'
+
