be960683e70c7289abff7d854150bb00eaab246c
kokossVN <manh12345678951235789@gmail.com>
2023-09-26 10:51:15 +0700

    add new feature for external screen

--- bash_customalias.sh
+++ bash_customalias.sh
@@ -1,5 +1,5 @@
 #!~/.bashrc
-
+CURRENT_DEVICE=desktop
 
 alias tmuxatk='tmux attach -t'
 alias vim='nvim'
@@ -14,3 +14,9 @@ alias tree="lsd --tree -X"
 alias lazyweb="~/dotfiles/custom_bash/lazyweb.sh"
 alias html="~/dotfiles/custom_bash/html.sh"
 alias live-server="live-server --browser=min"
+
+if [[ $CURRENT_DEVICE = desktop ]]
+then
+	alias system_blight="ddcutil setvcp 10"
+	alias system_cont="ddcutil setvcp 12"
+fi
