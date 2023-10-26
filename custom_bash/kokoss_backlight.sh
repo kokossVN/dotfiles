b635586daf3ca46471032b7e54f1988c8d8f81c8
kokossVN <manh12345678951235789@gmail.com>
2023-10-27 01:35:35 +0700

    smail change

--- custom_bash/kokoss_backlight.sh
+++ custom_bash/kokoss_backlight.sh
@@ -1,7 +1,35 @@
 #!/usr/bin/env bash
 
+if [[ $(echo $(hostnamectl) | grep -ic "laptop") -eq 1 ]]; then	
+	THIS_DEVICE=laptop
+elif [[ $(echo $(hostnamectl) | grep -ic "desktop") -eq 1 ]]; then
+	# THIS_DEVICE=desktop
+	THIS_DEVICE=laptop
+fi
 
+case $THIS_DEVICE in 
+	laptop)
+		echo "laptop"
+		if [[ ! $(whoami) = "root" ]]; then
+			echo "Permision Denined"
+			echo "Try again with root"
+		else
+			echo "on working"
+		fi
+		;;
+	desktop)
+		echo "desktop"
+		;;
+esac
 
-
-echo $(hostnamectl) | grep -ic	"laptop" 
-echo $(hostnamectl) | grep -ic	"desktop" 
+# if [[ $THIS_DEVICE = "laptop" ]]; then
+# fi
+#
+# if [[ $THIS_DEVICE = "desktop" ]]; then
+# 	ddcutil setvcp $1 $2 
+# 	ddcutil setvcp $1 $2
+# 	if [[ $1 -eq 10 ]]; then
+# 		echo "change brightness to $2"
+# 		else
+# 	fi
+# fi
