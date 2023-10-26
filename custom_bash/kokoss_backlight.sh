bbe90ae2cd7f8b28d7df41569e5b4247d7b4d81e
kokossVN <manh12345678951235789@gmail.com>
2023-10-27 02:07:17 +0700

    smail change

--- custom_bash/kokoss_backlight.sh
+++ custom_bash/kokoss_backlight.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
+
+#what is your pc
 if [[ $(echo $(hostnamectl) | grep -ic "laptop") -eq 1 ]]; then	
 	THIS_DEVICE=laptop
 elif [[ $(echo $(hostnamectl) | grep -ic "desktop") -eq 1 ]]; then
 	THIS_DEVICE=desktop
 fi
 
+
+
+#only number in parameter
+for i in $*; do
+	if [[ ! $i =~ ^[0-9]+$ ]]; then
+		echo "only number in value"
+		exit 1
+fi
+
+
+done
 case $THIS_DEVICE in 
 	laptop)
 		echo " this is laptop"
@@ -20,21 +33,13 @@ case $THIS_DEVICE in
 		echo " this is desktop"
 		case $1 in 
 			10)
-				echo "brightness change to $2 %"
+				echo "brightness change to $2%"
 				;;
 			12)
-				echo "contrast chane to $2 %"
+				echo "contrast chane to $2%"
+				;;
+			*)
+				echo "only id 10 and 12 can set value"
 		;;
+		esac
 esac
-
-# if [[ $THIS_DEVICE = "laptop" ]]; then
-# fi
-#
-# if [[ $THIS_DEVICE = "desktop" ]]; then
-# 	ddcutil setvcp $1 $2 
-# 	ddcutil setvcp $1 $2
-# 	if [[ $1 -eq 10 ]]; then
-# 		echo "change brightness to $2"
-# 		else
-# 	fi
-# fi
---
+++ custom_bash/num.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+
+if [[ $1 =~ ^[0-9]+$ ]]; then
+	echo "this number"
+	
+fi
