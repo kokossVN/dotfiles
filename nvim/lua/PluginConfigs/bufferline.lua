af1e1a5aa1ab8930c9389249c3c529200ccdd5c4
kokossVN <manh12345678951235789@gmail.com>
2023-11-26 20:30:37 +0700

    Smail change

--- nvim/lua/PluginConfigs/bufferline.lua
+++
@@ -1,6 +0,0 @@
-local bl = require('bufferline')
-bl.setup{
-	options = {
-		themable = true,
-	}
-}
--- nvim/lua/cmd.lua
+++
@@ -1,3 +0,0 @@
-vim.api.nvim_create_user_command("W", ":w", {})
-vim.api.nvim_create_user_command("Q", ":qa", {})
-
