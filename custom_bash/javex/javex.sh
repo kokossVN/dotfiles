df0e9f86f6f957e630b208e4dae60a31053ba76a
kokossVN <manh12345678951235789@gmail.com>
2023-12-21 22:51:19 +0700

    add run feature in javex

--- custom_bash/javex/javex.sh
+++ custom_bash/javex/javex.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
 JAVA_PROJECT_PATH="/home/kokoss/learn/java/proj"
+JAVA_PROJECT_BIN_PATH=$(pwd)
+JAVA_PROJECT_ROOT_PATH=${JAVA_PROJECT_BIN_PATH%*/bin}
+
 [[ $(pwd) != $JAVA_PROJECT_PATH* ]] && echo "This is not Java project" && exit 1
 [[ $(pwd) != *bin* ]] && echo "only work in root/bin" && exit 1
-[[ $1 = build ]] && javac -d . ../src/*.java ../src/*/*.java && echo "Done!"
-[[ $1 = ls ]] && tree
+[[ $@ = *build* ]] && javac -d . ../src/*.java &&
+	[[ $(find $JAVA_PROJECT_ROOT_PATH/src/* -type d) ]] && echo "Detected package" && javac -d . ../src/*/*.java && echo "Done!" ||
+	echo "Done!"
+
+[[ $@ = *run* ]] && (test -e $(pwd)/App.* && java App) || echo "Only if App.class exist!"
+
+[[ $1 = tree ]] && tree
 
 exit 0
