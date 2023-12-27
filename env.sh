adda512d76f9129e27dbeb5fb84ddd247622bfc2
kokossVN <manh12345678951235789@gmail.com>
2023-12-27 19:34:08 +0700

    Smail change

--- env.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export DOTFILES_CUSTOM_BASH="/home/$(whoami)/dotfiles/custom_bash"
-export DOTFILES="/home/$(whoami)/dotfiles"
-export GITHUB_TOKEN="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"
