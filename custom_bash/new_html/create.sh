1ddf4bbe7c104e71ae09872830e24d6bbc398fe4
kokossVN <manh12345678951235789@gmail.com>
2024-01-29 07:35:35 +0700

    Smail change

--- custom_bash/new_html/create.sh
+++ custom_bash/new_html/create.sh
@@ -1,51 +1,47 @@
 #!/usr/bin/env bash
 
-
-
 source "$this_location/from_template.sh"
 # custom title
 custom_title() {
 	read -p "Title name:" title
 }
 
-
 #name check
 file_name_check() {
-	if [[ $last = *.html ]]; then
+	if [[ $last = *.html || $last = *.php ]]; then
 		OUTPUT=$last
 	fi
 	if [[ -z $1 ]]; then
 		if [[ -z $OUTPUT ]]; then
-			read -p "File name: " 
+			read -p "File name [*.html]: "
 			file_output_check $REPLY
 		fi
 	else
-	file_output_check $1
+		file_output_check $1
 	fi
 }
 
-#push func should be shift until next cmd parameter or 
+#push func should be shift until next cmd parameter or
 #last element, this function will take lastest array
 #for output check
 double_double_check() {
-	for last in $@ ; do
+	for last in $@; do
 		true
 	done
 	echo " parameter $last"
 }
 
-#ask for name 
+#ask for name
 #its exit error if nothing in
 file_output_check() {
-	if [[ $1 = *.html ]]; then
+	if [[ $1 = *.html || $1 = *.php ]]; then
 		OUTPUT="$1"
-	elif [[
+	elif [[ 
 		$1 = *.css ||
-		$1 = *.js
-		]];
-		then
-			echo "WTF you mean?"
-			exit 1
+		$1 = *.js ]] \
+		; then
+		echo "WTF you mean?"
+		exit 1
 	else
 		OUTPUT="$1.html"
 	fi
@@ -56,14 +52,15 @@ file_output_check() {
 #with yzal tool
 html_create() {
 	if [[ -z $title ]]; then
-		title=$(echo "${OUTPUT%*.html}") 
+		title=$(echo "${OUTPUT%*.html}")
+		title=$(echo "${OUTPUT%*.php}")
 	fi
-	if [[ ! -z $TEMPLATE_FILE  ]]; then
+	if [[ ! -z $TEMPLATE_FILE ]]; then
 		# echo "Template help me do this sh*t"
 		template_do
 	else
 		echo \
-		"<!DOCTYPE html>
+			"<!DOCTYPE html>
 		<html lang="en">
 		<head>
 			<meta charset="UTF-8">
@@ -77,19 +74,19 @@ html_create() {
 			$js
 			<!-- K_js_in -->
 		</body>
-		</html>" > $OUTPUT
+		</html>" >$OUTPUT
 	fi
 }
 
 ask_for_open_nvim() {
 	read -p "wanna open vim [yY*|nN] " aws
 	case $aws in
-		n|N)
-			exit 0
-			;;
-		y|Y|*)
-			nvim $OUTPUT
-			;;
+	n | N)
+		exit 0
+		;;
+	y | Y | *)
+		nvim $OUTPUT
+		;;
 	esac
 
 }
