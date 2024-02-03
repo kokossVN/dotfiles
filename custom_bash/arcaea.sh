a53eb280e85e45532e54f40611a0aaed7f1f5529
kokossVN <manh12345678951235789@gmail.com>
2024-02-03 15:41:40 +0700

    refactor: 💡 change smt but still useless

--- custom_bash/arcaea.sh
+++ custom_bash/arcaea.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
-export QT_FONT_DPI=96 && yuzu -f -g /home/kokoss/games/Arcaea/arcaea2/arcaea.nsp
+echo "make sure every thing ok"
+# export QT_FONT_DPI=96 && yuzu -f -g /home/kokoss/games/Arcaea/arcaea2/arcaea.nsp
--- preload.sh
+++
@@ -1,7 +0,0 @@
-dosmt() {
-	echo "loaded"
-	tmux new -d tmuxifier w smt
-	PRELOADED=1
-	export PRELOADED
-}
-[[ $(env | grep PRELOADED) = 0 ]] && dosmt
